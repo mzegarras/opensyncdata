716f2427bada3c6a2d15fa7a005f2702d0404e75
manuelzegarra <mzegarra@gmail.com>
2016-01-30 16:31:18 -0500

    Domain data

---
+++ net.msonic.sync/net.msonic.sync.domain/src/main/java/net/msonic/sync/App.java
@@ -0,0 +1,13 @@
+package net.msonic.sync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
